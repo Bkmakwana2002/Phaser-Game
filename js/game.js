7c9e3fe51befeeca35e2987adf43e137e63c1c3c
Bkmakwana2002 <2020meb1278@iitrpr.ac.im>
2023-05-18 00:30:08 +0530

    first

---
+++ js/game.js
@@ -0,0 +1,36 @@
+import TitleScene from './titleScene.js'
+import MenuScene from './menuScene.js'
+import GameScene from './gameScene.js'
+
+const titleScene = new TitleScene()
+const menuScene = new MenuScene()
+const gameScene = new GameScene()
+
+
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false
+    }
+  },
+  
+  backgroundColor: 0xffffff,
+  scale: {
+    mode: Phaser.Scale.FIT,
+
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
+}
+
+const game = new Phaser.Game(config)
+
+game.scene.add('titleScene', titleScene)
+game.scene.add('menuScene', menuScene)
+game.scene.add('gameScene', gameScene)
+
+// start title
+game.scene.start('titleScene')
